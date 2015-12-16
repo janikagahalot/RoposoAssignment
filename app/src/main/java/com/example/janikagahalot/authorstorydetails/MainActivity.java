93b288ccaa75d92f3058af01ccc4b7487c2e89e0
Janika Gahalot <janikagahalot@gmail.com>
2015-12-17 05:17:33 +0530

    NotifyItemChanged For Specific Position Added

--- app/src/main/java/com/example/janikagahalot/authorstorydetails/MainActivity.java
+++ app/src/main/java/com/example/janikagahalot/authorstorydetails/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.janikagahalot.authorstorydetails;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.janikagahalot.authorstorydetails.model.AuthorDetails;
 import com.example.janikagahalot.authorstorydetails.model.PreFetchLayoutManager;
@@ -17,13 +16,15 @@ import com.google.gson.JsonParser;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements AuthorDetailsAdapter.onFollowButtonClickListener {
 
     private AuthorDetails[] details;
     private RecyclerView mRecyclerView;
     private AuthorDetailsAdapter authorDetailsAdapter;
+    private HashMap<String, ArrayList<Integer>> commons = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements AuthorDetailsAdap
             inputStream.close();
             json = new String(buffer, "UTF-8");
             details  = new Gson().fromJson(json, AuthorDetails[].class);
+            updateCommons();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return;
@@ -47,65 +49,66 @@ public class MainActivity extends AppCompatActivity implements AuthorDetailsAdap
             return;
         }
         mRecyclerView = (RecyclerView) findViewById(R.id.author_details_list);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new PreFetchLayoutManager(this));
         authorDetailsAdapter = new AuthorDetailsAdapter(this, details, this);
         mRecyclerView.setAdapter(authorDetailsAdapter);
     }
 
-
+    private void updateCommons() {
+        for(Integer i = 1; i < details.length; i++){
+            String key = details[i].getDb();
+            if(commons.containsKey(key)){
+                commons.get(key).add(i);
+            }
+            else {
+                ArrayList<Integer> list  = new ArrayList<>();
+                list.add(i);
+                commons.put(key, list);
+            }
+        }
+    }
 
     @Override
-    public void onFollowButton(boolean following,HashSet<Integer> positions, AuthorDetails authorDetail){
-        details[0].setIsFollowing(following);
-        for(int i= 1; i< details.length; i++) {
-            if (details[i].getDb().equals(authorDetail.getDb())) {
-                details[i].setIsFollowing(following);
-            }
+    public void onFollowAll(boolean following) {
+        onFollowButton(following, null);
+    }
+
+    @Override
+    public void onFollowButton(boolean following,String key){
             if(following) {
                 String text = getString(R.string.following);
                 Drawable drawable = getDrawable(R.drawable.followed_button_background);
-                updateButtons(positions, text, drawable);
+                updateButtons(following,key, text, drawable);
             }
             else {
                 String text = getString(R.string.follow);
                 Drawable drawable = getDrawable(R.drawable.follow_button_background);
-                updateButtons(positions, text, drawable);
+                updateButtons(following, key, text, drawable);
             }
-
-        }
-    }
-@Override
-public void onAuthorButtonClicked(boolean following, HashSet<Integer> set){
-    for(int i= 1; i< details.length; i++) {
-            details[i].setIsFollowing(following);
-        }
-    if(following) {
-        String text = getString(R.string.following);
-        Drawable drawable = getDrawable(R.drawable.followed_button_background);
-        updateButtons(set, text, drawable);
-    }
-    else {
-        String text = getString(R.string.follow);
-        Drawable drawable = getDrawable(R.drawable.follow_button_background);
-        updateButtons(set, text, drawable);
     }
 
-}
-
-
-    private void updateButtons(HashSet<Integer> positions, String text, Drawable drawable){
-        LinearLayoutManager pflm  = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+    private void updateButtons(boolean following, String key, String text, Drawable drawable) {
+        GridLayoutManager pflm  = (GridLayoutManager) mRecyclerView.getLayoutManager();
         int first = pflm.findFirstVisibleItemPosition();
         int last =  pflm.findLastVisibleItemPosition();
-       // int pos= 0;
-               for (int pos : positions) {
-                   if (pos-1 >= first && pos-1 <= last) {
-                       View view = pflm.findViewByPosition(pos-1);
-                       Button button = (Button) view.findViewById(R.id.user_follow_button);
-                       button.setText(text);
-                       button.setBackground(drawable);
-                   }
-               }
+        ArrayList<Integer> positions = new ArrayList<>();
+        if(key == null) {
+            for(Integer  i = 0; i< details.length; i++){
+                positions.add(i);
+            }
+        }
+        else {
+            positions = commons.get(key);
+        }
+        for (int pos : positions) {
+            authorDetailsAdapter.notifyItemChanged(pos);
+            details[pos].setIsFollowing(following);
+            if (pos >= first && pos <= last) {
+                View view = pflm.findViewByPosition(pos);
+                Button button = (Button) view.findViewById(R.id.user_follow_button);
+                button.setText(text);
+                button.setBackground(drawable);
+            }
+        }
     }
-
 }
